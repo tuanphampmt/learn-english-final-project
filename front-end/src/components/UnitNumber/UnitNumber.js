a6baaa543f24f2d4d1e91e61ff1815c409077cb8
Tuan Pham <tuanpham31798@gmail.com>
2021-05-26 23:03:30 +0700

    aaa

--- front-end/src/components/UnitNumber/UnitNumber.js
+++ front-end/src/components/UnitNumber/UnitNumber.js
@@ -380,7 +380,7 @@ class UnitNumber extends Component {
                     <div id="js-panel" className="panel" style={{display: "block"}}>
                         <div className="panel__content">
                             <h4>Bắt đầu</h4>
-                            <p> Bạn hãy bắt đầu trò chơi</p>
+                            <p style={{fontSize: "1.5em"}}> Bạn hãy nghe và chọn câu trả lời đúng nhất.</p>
                         </div>
                         <div className="panel__flaps">
                             <div className="flap outer flap--left"/>
