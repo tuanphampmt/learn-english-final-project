a12b374585614c0ce9ea429b874896356fb100b5
HThuong <HoaiThuongdtu231099@gmail.com>
2021-05-27 22:50:23 +0700

    update

--- front-end/src/components/Unit_Color/UnitColor.js
+++ front-end/src/components/Unit_Color/UnitColor.js
@@ -1,41 +1,238 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 class UnitColor extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            url: "",
-            colorsYesPlay: ["green", "orange", "red", "pink", "purple", "yellow", "gray", "blue", "black", "brown"]
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: "/Images/Unit Color/Game_Card/Pig/NoColor_Pig.png",
+      colors: [
+        "Green",
+        "Orange",
+        "Red",
+        "Pink",
+        "Purple",
+        "Yellow",
+        "Blue",
+        "Black",
+        "Brown",
+        "White",
+      ],
+      ids: [
+        "Green",
+        "Orange",
+        "Red",
+        "Pink",
+        "Purple",
+        "Yellow",
+        "Blue",
+        "Black",
+        "Brown",
+        "White",
+      ],
+      color: "",
+      urls: [
+        {
+          code: "Green",
+          name: "Images/Unit Color/Game_Card/Pig/Green_Pig.png",
+        },
+        {
+          code: "Orange",
+          name: "Images/Unit Color/Game_Card/Pig/Orange_Pig.png",
+        },
+        { code: "Red", name: "Images/Unit Color/Game_Card/Pig/Red_Pig.png" },
+        { code: "Pink", name: "Images/Unit Color/Game_Card/Pig/Pink_Pig.png" },
+        {
+          code: "Purple",
+          name: "Images/Unit Color/Game_Card/Pig/Purple_Pig.png",
+        },
+        {
+          code: "Yellow",
+          name: "Images/Unit Color/Game_Card/Pig/Yellow_Pig.png",
+        },
+        { code: "Blue", name: "Images/Unit Color/Game_Card/Pig/Blue_Pig.png" },
+        {
+          code: "Black",
+          name: "Images/Unit Color/Game_Card/Pig/Black_Pig.png",
+        },
+        {
+          code: "Brown",
+          name: "Images/Unit Color/Game_Card/Pig/Brown_Pig.png",
+        },
+        {
+          code: "White",
+          name: "Images/Unit Color/Game_Card/Pig/White_Pig.png",
+        },
+      ],
+      isCorrectAnswer: false,
+      isWin: false,
+      score: 0,
+      countdown: null,
+      start: false,
+      isHidden: true,
+      minutes: 0,
+      seconds: "00",
+      number: "",
+      gameOver: false
+    };
+  }
   toSpeak = (message) => {
     var speechSynthesisUtterance = new SpeechSynthesisUtterance(message);
     speechSynthesisUtterance.lang = "en-UK";
     window.speechSynthesis.speak(speechSynthesisUtterance);
   };
 
-  playAudio = (color) => {
-    if (this.props.code === color) {
-        this.toSpeak("Yes")
-    } else {
-        this.toSpeak("No")
+  random = (arr, n) => {
+    var result = new Array(n),
+      len = arr.length,
+      taken = new Array(len);
+    if (n > len)
+      throw new RangeError("getRandom: more elements taken than available");
+    while (n--) {
+      var x = Math.floor(Math.random() * len);
+      result[n] = arr[x in taken ? taken[x] : x];
+      taken[x] = --len in taken ? taken[len] : len;
     }
-}
+    return result;
+  };
+
+  playAudio = (color) => {
+    if (this.state.colors.length !== 0) {
+      const e = document.getElementById(color);
+      if (e) {
+        console.log(e.style.cursor);
+        
+        if (e.style.cursor === "pointer") {
+          if (this.state.color === color) {
+            this.toSpeak("Yes");
+            this.setState({
+              score: this.state.score + 10,
+              isCorrectAnswer: false,
+            });
+            var url = this.state.urls.find((item) => item.code === color);
+            if (url) {
+              this.setState({ url: url.name });
+            }
+            var colors = this.state.colors.filter((i) => i !== color);
+            this.setState({
+              colors,
+            });
+            var urls = this.state.urls.filter((item) => item.code !== color);
+            this.setState({ urls });
+
+            for (let i = 0; i < this.state.ids.length; i++) {
+              const e = document.getElementById(this.state.ids[i]);
+              if (e) {
+                e.style.cursor = "not-allowed";
+              }
+            }
+            const wait = (ms) =>
+              new Promise((resolve) => setTimeout(resolve, ms));
+            Promise.resolve(3000)
+              .then(() => wait(3000))
+              .then(() => {
+                if (this.state.colors.length !== 0) {
+                  var color = this.random(this.state.colors, 1)[0];
+                  this.setState({
+                    color,
+                    url: "/Images/Unit Color/Game_Card/Pig/NoColor_Pig.png",
+                  });
+                  this.toSpeak(color);
+                  for (let i = 0; i < this.state.ids.length; i++) {
+                    const e = document.getElementById(this.state.ids[i]);
+                    if (e) {
+                      e.style.cursor = "pointer";
+                    }
+                  }
+                }
+                // this.toSpeak(presentNumber);
+                // this.setState({ ...answers });
+                // this.init();
+              });
+          } else {
+            this.toSpeak("No");
+            var colors = this.state.colors.filter((i) => i !== color);
+            this.setState({
+              colors,
+            });
+            var urls = this.state.urls.filter((item) => item.code !== color);
+            this.setState({ urls });
+
+            for (let i = 0; i < this.state.ids.length; i++) {
+              const e = document.getElementById(this.state.ids[i]);
+              if (e) {
+                e.style.cursor = "not-allowed";
+              }
+            }
+            const wait = (ms) =>
+              new Promise((resolve) => setTimeout(resolve, ms));
+            Promise.resolve(3000)
+              .then(() => wait(3000))
+              .then(() => {
+                if (this.state.colors.length !== 0) {
+                  var color = this.random(this.state.colors, 1)[0];
+                  this.setState({
+                    color,
+                    url: "/Images/Unit Color/Game_Card/Pig/NoColor_Pig.png",
+                  });
+                  this.toSpeak(color);
+                  for (let i = 0; i < this.state.ids.length; i++) {
+                    const e = document.getElementById(this.state.ids[i]);
+                    if (e) {
+                      e.style.cursor = "pointer";
+                    }
+                  }
+                }
+
+                // this.toSpeak(presentNumber);
+                // this.setState({ ...answers });
+                // this.init();
+              });
+          }
+        }
+      }
+    } else {
+      this.setState({ score: this.state.score, isWin: true });
+      clearInterval(this.state.countdown);    
+    }
+  };
+
+  componentDidMount() {
+    var color = this.random(this.state.colors, 1)[0];
+    this.setState({ color });
+    this.toSpeak(color);
+    for (let i = 0; i < this.state.ids.length; i++) {
+      const e = document.getElementById(this.state.ids[i]);
+      if (e) {
+        e.style.cursor = "pointer";
+      }
+    }
+
+    sessionStorage.setItem("colors", JSON.stringify(this.state.colors))
+    sessionStorage.setItem("urls", JSON.stringify(this.state.urls))
+  }
+
+  
   render() {
     return (
       <div className="container unit-aphabet">
-        {/* {this.state.isWin && (
+        {this.state.isWin && (
           <div id="js-panel" className="panel" style={{ display: "block" }}>
             <div className="panel__content">
-              <h4 id="notify"></h4>
+              <h4 id="notify" style={{ color: "#28a745" }}>
+                Chúc mừng
+              </h4>
               <h2>
-                Bạn đạt <span id="point-game">{this.state.point}</span> điểm
+                Bạn đạt <span id="score-game">{this.state.score}</span> điểm
               </h2>
-              <p id="description"></p>
+              <p id="description">Bạn được nhận thêm điểm kinh nghiệm</p>
             </div>
             <div className="panel__flaps">
               <div className="flap outer flap--left" />
-              <Link className="flap flap__btn" id="play-again">
+              <Link
+                className="flap flap__btn"
+                id="play-again"
+                onClick={() => this.countdown()}
+              >
                 Chơi lại
               </Link>
               <Link to="/home-page" className="flap flap__btn" id="exit">
@@ -44,51 +241,108 @@ class UnitColor extends Component {
               <div className="flap outer flap--right" />
             </div>
           </div>
-        )} */}
-        {/* <div className="overlay-text visible" id="countdown">
-          <div className="demo">
-            <div className="demo__colored-blocks">
-              <div className="demo__colored-blocks-rotater">
-                <div className="demo__colored-block" />
-                <div className="demo__colored-block" />
-                <div className="demo__colored-block" />
-              </div>
-              <div className="demo__colored-blocks-inner" />
-              <div className="demo__text">Ready</div>
+        )}
+
+        {this.state.gameOver && (
+          <div id="js-panel" className="panel" style={{ display: "block" }}>
+            <div className="panel__content">
+              <h4 id="notify" style={{ color: "#dc3545" }}>
+                Hết giờ"
+              </h4>
+              <h2>
+                Bạn đạt <span id="score-game">{this.state.score}</span> điểm
+              </h2>
+              <p id="description"></p>
             </div>
-            <div className="demo__inner">
-              <svg className="demo__numbers" viewBox="0 0 100 100">
-                <defs>
-                  <path className="demo__num-path-1" d="M40,28 55,22 55,78" />
-                  <path
-                    className="demo__num-join-1-2"
-                    d="M55,78 55,83 a17,17 0 1,0 34,0 a20,10 0 0,0 -20,-10"
-                  />
-                  <path
-                    className="demo__num-path-2"
-                    d="M69,73 l-35,0 l30,-30 a16,16 0 0,0 -22.6,-22.6 l-7,7"
-                  />
-                  <path
-                    className="demo__num-join-2-3"
-                    d="M28,69 Q25,44 34.4,27.4"
-                  />
-                  <path
-                    className="demo__num-path-3"
-                    d="M30,20 60,20 40,50 a18,15 0 1,1 -12,19"
-                  />
-                </defs>
-                <path
-                  className="demo__numbers-path"
-                  d="M-10,20 60,20 40,50 a18,15 0 1,1 -12,19
-                Q25,44 34.4,27.4
-                l7,-7 a16,16 0 0,1 22.6,22.6 l-30,30 l35,0 L69,73 
-                a20,10 0 0,1 20,10 a17,17 0 0,1 -34,0 L55,83 
-                l0,-61 L40,28"
-                />
-              </svg>
+            <div className="panel__flaps">
+              <div className="flap outer flap--left" />
+              <Link
+                className="flap flap__btn"
+                id="play-again"
+                onClick={() => this.gameOverCountdown()}
+              >
+                Chơi lại
+              </Link>
+              <Link to="/home-page" className="flap flap__btn" id="exit">
+                Thoát
+              </Link>
+              <div className="flap outer flap--right" />
             </div>
           </div>
-        </div> */}
+        )}
+
+        {/* {!this.state.start && (
+          <div id="js-panel" className="panel" style={{ display: "block" }}>
+            <div className="panel__content">
+              <h4>Bắt đầu</h4>
+              <p style={{ fontSize: "1.5em" }}>
+                {" "}
+                Bạn hãy nghe và chọn câu trả lời đúng nhất.
+              </p>
+            </div>
+            <div className="panel__flaps">
+              <div className="flap outer flap--left" />
+              <Link
+                className="flap flap__btn"
+                id="play-again"
+                onClick={() => this.countdown()}
+              >
+                Bắt đầu
+              </Link>
+              <Link to="/home-page" className="flap flap__btn" id="exit">
+                Thoát
+              </Link>
+              <div className="flap outer flap--right" />
+            </div>
+          </div>
+        )} */}
+
+        {this.state.start && (
+          <div className="overlay-text visible" id="countdown">
+            <div className="demo">
+              <div className="demo__colored-blocks">
+                <div className="demo__colored-blocks-rotater">
+                  <div className="demo__colored-block" />
+                  <div className="demo__colored-block" />
+                  <div className="demo__colored-block" />
+                </div>
+                <div className="demo__colored-blocks-inner" />
+                <div className="demo__text">Ready</div>
+              </div>
+              <div className="demo__inner">
+                <svg className="demo__numbers" viewBox="0 0 100 100">
+                  <defs>
+                    <path className="demo__num-path-1" d="M40,28 55,22 55,78" />
+                    <path
+                      className="demo__num-join-1-2"
+                      d="M55,78 55,83 a17,17 0 1,0 34,0 a20,10 0 0,0 -20,-10"
+                    />
+                    <path
+                      className="demo__num-path-2"
+                      d="M69,73 l-35,0 l30,-30 a16,16 0 0,0 -22.6,-22.6 l-7,7"
+                    />
+                    <path
+                      className="demo__num-join-2-3"
+                      d="M28,69 Q25,44 34.4,27.4"
+                    />
+                    <path
+                      className="demo__num-path-3"
+                      d="M30,20 60,20 40,50 a18,15 0 1,1 -12,19"
+                    />
+                  </defs>
+                  <path
+                    className="demo__numbers-path"
+                    d="M-10,20 60,20 40,50 a18,15 0 1,1 -12,19
+                Q25,44 34.4,27.4
+                l7,-7 a16,16 0 0,1 22.6,22.6 l-30,30 l35,0 L69,73
+                a20,10 0 0,1 20,10 a17,17 0 0,1 -34,0 L55,83
+                l0,-61 L40,28"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
+        )}
         <div
           className="row center"
           style={{ display: "flex", justifyContent: "space-between" }}
@@ -116,7 +370,7 @@ class UnitColor extends Component {
                 <span id="Timer">Thời gian: 2:00</span>
               </div>
               <div className="point col-md-3">
-                <span id="Point">Điểm: 0</span>
+        <span id="Point">Điểm: {this.state.score}</span>
               </div>
               <div className="col-md-1"></div>
             </div>
@@ -128,8 +382,11 @@ class UnitColor extends Component {
               <img
                 src="/Images/Unit Color/Game_Card/Pen/Red_Pen.png"
                 alt=""
+                style={{ cursor: "pointer" }}
+                id="Red"
                 width="100px"
                 height="150px"
+                onClick={() => this.playAudio("Red")}
               />
             </a>
             <a>
@@ -137,8 +394,11 @@ class UnitColor extends Component {
                 src="/Images/Unit Color/Game_Card/Pen/Yellow_Pen.png"
                 alt=""
                 width="100px"
+                id="Yellow"
+                style={{ cursor: "pointer" }}
                 height="150px"
                 style={{ marginLeft: "50px" }}
+                onClick={() => this.playAudio("Yellow")}
               />
             </a>
             <a>
@@ -146,8 +406,11 @@ class UnitColor extends Component {
                 src="/Images/Unit Color/Game_Card/Pen/Brown_Pen.png"
                 alt=""
                 width="100px"
+                id="Brown"
+                style={{ cursor: "pointer" }}
                 height="150px"
                 style={{ marginLeft: "50px" }}
+                onClick={() => this.playAudio("Brown")}
               />
             </a>
             <br />
@@ -156,7 +419,10 @@ class UnitColor extends Component {
                 src="/Images/Unit Color/Game_Card/Pen/Green_Pen.png"
                 alt=""
                 width="100px"
+                id="Green"
+                style={{ cursor: "pointer" }}
                 height="150px"
+                onClick={() => this.playAudio("Green")}
               />
             </a>
             <a>
@@ -164,17 +430,23 @@ class UnitColor extends Component {
                 src="/Images/Unit Color/Game_Card/Pen/Pink_Pen.png"
                 alt=""
                 width="100px"
+                id="Pink"
+                style={{ cursor: "pointer" }}
                 height="150px"
                 style={{ marginLeft: "50px" }}
+                onClick={() => this.playAudio("Pink")}
               />
             </a>
             <a>
               <img
                 src="/Images/Unit Color/Game_Card/Pen/White_Pen.png"
                 alt=""
+                id="White"
                 width="100px"
                 height="150px"
+                style={{ cursor: "pointer" }}
                 style={{ marginLeft: "50px" }}
+                onClick={() => this.playAudio("White")}
               />
             </a>
             <a>
@@ -182,8 +454,11 @@ class UnitColor extends Component {
                 src="/Images/Unit Color/Game_Card/Pen/Orange_Pen.png"
                 alt=""
                 width="100px"
+                id="Orange"
                 height="150px"
+                style={{ cursor: "pointer" }}
                 style={{ marginLeft: "50px" }}
+                onClick={() => this.playAudio("Orange")}
               />
             </a>
             <br />
@@ -192,7 +467,10 @@ class UnitColor extends Component {
                 src="/Images/Unit Color/Game_Card/Pen/Blue_Pen.png"
                 alt=""
                 width="100px"
+                id="Blue"
+                style={{ cursor: "pointer" }}
                 height="150px"
+                onClick={() => this.playAudio("Blue")}
               />
             </a>
             <a>
@@ -200,8 +478,11 @@ class UnitColor extends Component {
                 src="/Images/Unit Color/Game_Card/Pen/Purple_Pen.png"
                 alt=""
                 width="100px"
+                id="Purple"
+                style={{ cursor: "pointer" }}
                 height="150px"
                 style={{ marginLeft: "50px" }}
+                onClick={() => this.playAudio("Purple")}
               />
             </a>
             <a>
@@ -209,24 +490,28 @@ class UnitColor extends Component {
                 src="/Images/Unit Color/Game_Card/Pen/Black_Pen.png"
                 alt=""
                 width="100px"
+                id="Black"
                 height="150px"
+                style={{ cursor: "pointer" }}
                 style={{ marginLeft: "50px" }}
+                onClick={() => this.playAudio("Black")}
               />
             </a>
           </div>
           <div
             className="col-sm-4 game-number-row"
             id="game-number-row-question"
-            style={{marginLeft: "-100px", textAlign: 'center' }}
+            style={{ marginLeft: "-100px", textAlign: "center" }}
           >
             <a>
               <img
-                src="/Images/Unit Color/Game_Card/Pig/NoColor_Pig.png"
+                src={this.state.url}
                 width="100%"
-                onClick={() => this.toSpeak("yellow")}
+                style={{ cursor: "pointer" }}
+                onClick={() => this.toSpeak(this.state.color)}
               ></img>
             </a>
-            <h2 style={{fontSize: "80px"}}>Yellow</h2>
+            <h2 style={{ fontSize: "80px" }}>{this.state.color}</h2>
           </div>
         </div>
       </div>
