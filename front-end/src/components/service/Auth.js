fa2b9658cf6000312ba91efbf457ca479ded8bf8
HThuong <HoaiThuongdtu231099@gmail.com>
2021-06-06 20:58:50 +0700

    fix-login

--- front-end/src/components/service/Auth.js
+++ front-end/src/components/service/Auth.js
@@ -1,46 +1,51 @@
 import axios from "axios";
 
-// const API_URL = "https://backend-kide.herokuapp.com/api/";
+const API_URL = "https://backend-kide.herokuapp.com/api/signin";
 
 class Auth {
-    login(username, password) {
-        return axios
-            .post("https://backend-kide.herokuapp.com/api/signin", {
-                    username,
-                    password
-                },
-                {
-                    headers: {
-                        'Access-Control-Allow-Origin': '*',
-                        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-                        "Content-Type": "application/x-www-form-urlencoded"
-                    }
-                })
-            .then(response => {
-                if (response.data.accessToken) {
-                    localStorage.setItem("user", JSON.stringify(response.data));
-                }
+  login(username, password) {
+    return axios
+      .post(
+        API_URL,
+        {
+          username,
+          password,
+        },
+        {
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS", 
+          },
+        }
+      )
+      .then((response) => {
+        if (response.data.accessToken) {
+          localStorage.setItem("user", JSON.stringify(response.data));
+          localStorage.setItem(
+            "jwt",
+            JSON.stringify(response.data.accessToken)
+          );
+        }
 
-                return response.data;
-            });
-    }
+        return response.data;
+      });
+  }
 
-    logout() {
-        localStorage.removeItem("user");
-        localStorage.removeItem("avatar");
-    }
+  logout() {
+    localStorage.removeItem("user");
+    localStorage.removeItem("avatar");
+  }
 
-    register(username, password, role) {
-        return axios.post("https://backend-kide.herokuapp.com/api/signup", {
-            username,
-            password
-        });
-    }
+  register(username, password, role) {
+    return axios.post("https://backend-kide.herokuapp.com/api/signup", {
+      username,
+      password,
+    });
+  }
 
-    getCurrentUser() {
-        return JSON.parse(localStorage.getItem('user'));
-    }
-
+  getCurrentUser() {
+    return JSON.parse(localStorage.getItem("user"));
+  }
 }
 
 export default new Auth();
